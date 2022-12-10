ba44111c97fcf25da47891978db0decc44eb2a09
Doncho Donev <donevdoncho92@gmail.com>
2022-12-10 20:30:29 +0200

    add auth code flow with msal

--- appointments/src/App.tsx
+++ appointments/src/App.tsx
@@ -4,11 +4,75 @@ import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import Navigation from "./components/Navigation/Navigation";
+import { useIsAuthenticated, useMsal } from "@azure/msal-react";
+import { useEffect, useState } from "react";
+import {
+  InteractionRequiredAuthError,
+  InteractionStatus,
+} from "@azure/msal-browser";
 
 function App() {
+  const Button: React.FC = () => (
+    <button onClick={callApiAsync}>Call weather api</button>
+  );
+
+  const isAuthenticated = useIsAuthenticated();
+  const { instance, inProgress: interactionStatus, accounts } = useMsal();
+  const [accessToken, setAccessToken] = useState<string | undefined>();
+  const accessTokenRequest = {
+    scopes: [`api://${process.env.REACT_APP_SERVER_ID}/access_as_user`],
+    account: accounts[0],
+  };
+
+  const callApiAsync = () => {
+    var headers = new Headers();
+    var bearer = "Bearer " + accessToken;
+
+    headers.append("Authorization", bearer);
+    var options = {
+      method: "GET",
+      headers: headers,
+    };
+    var weatherEndpoint = "https://localhost:7067/WeatherForecast";
+
+    fetch(weatherEndpoint, options).then(console.log).catch(console.log);
+  };
+
+  const ensureAccessTokenExists = (interactionStatus: InteractionStatus) => {
+    if (interactionStatus === InteractionStatus.None && !isAuthenticated) {
+      console.log("inside unauth flow");
+      instance.loginPopup().then((authResult) => {
+        setAccessToken(authResult.accessToken);
+      });
+    } else if (
+      interactionStatus === InteractionStatus.None &&
+      isAuthenticated
+    ) {
+      instance
+        .acquireTokenSilent(accessTokenRequest)
+        .then((res) => setAccessToken(res.accessToken))
+        .catch((error) => {
+          if (error instanceof InteractionRequiredAuthError) {
+            instance
+              .acquireTokenPopup(accessTokenRequest)
+              .then((res) => setAccessToken(res.accessToken));
+          }
+        });
+    }
+  };
+
+  useEffect(() => {
+    ensureAccessTokenExists(interactionStatus);
+  }, [interactionStatus]);
+
   return (
     <>
       <Navigation pages={[]} settings={["Logout"]} />
+      <div className="App">
+        <header className="App-header">
+          {isAuthenticated ? <Button /> : <p>User is not logged in</p>}
+        </header>
+      </div>
     </>
   );
 }
--- appointments/src/index.tsx
+++ appointments/src/index.tsx
@@ -3,13 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Configuration, PublicClientApplication } from '@azure/msal-browser';
+import { MsalProvider } from '@azure/msal-react';
+import { msalConfig } from './authConfig';
+
+const publicClientApplication = new PublicClientApplication(msalConfig as Configuration);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <MsalProvider instance={publicClientApplication}>
+      <App />
+    </MsalProvider>
   </React.StrictMode>
 );
 
