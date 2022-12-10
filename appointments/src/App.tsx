3bd9a77c5eeca77a9dc4ba0df96fa6be7a62f1b9
Doncho Donev <donevdoncho92@gmail.com>
2022-12-10 17:01:32 +0200

    add navbar

--- appointments/src/App.tsx
+++ appointments/src/App.tsx
@@ -1,13 +1,15 @@
-import './App.css';
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
+import "./App.css";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import Navigation from "./components/Navigation/Navigation";
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <>
+      <Navigation pages={[]} settings={["Logout"]} />
+    </>
   );
 }
 
