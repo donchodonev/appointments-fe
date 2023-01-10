766dc5f453ec7401658c0a2faac88cc4320ceab6
Doncho Donev <donevdoncho92@gmail.com>
2023-01-10 09:20:40 +0200

    install react-router use login state in Navigation component

--- appointments/src/components/Navigation/Navigation.tsx
+++ appointments/src/components/Navigation/Navigation.tsx
@@ -4,21 +4,14 @@ import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import IconButton from "@mui/material/IconButton";
-import MenuIcon from "@mui/icons-material/Menu";
 import AccountCircle from "@mui/icons-material/AccountCircle";
-import Switch from "@mui/material/Switch";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import FormGroup from "@mui/material/FormGroup";
 import MenuItem from "@mui/material/MenuItem";
 import Menu from "@mui/material/Menu";
+import { useAppSelector } from "../../hooks/useAppSelector";
 
 export default function Navigation() {
-  const [auth, setAuth] = React.useState(true);
+  const isLoggedIn = useAppSelector((state) => state.auth.isLoggedIn );
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setAuth(event.target.checked);
-  };
 
   const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorEl(event.currentTarget);
@@ -30,24 +23,12 @@ export default function Navigation() {
 
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <FormGroup>
-        <FormControlLabel
-          control={
-            <Switch
-              checked={auth}
-              onChange={handleChange}
-              aria-label="login switch"
-            />
-          }
-          label={auth ? "Logout" : "Login"}
-        />
-      </FormGroup>
       <AppBar position="static">
         <Toolbar>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            {auth ? "Hi NAME" : ""}
+            {isLoggedIn ? "Hi NAME" : ""}
           </Typography>
-          {auth && (
+          {isLoggedIn && (
             <div>
               <IconButton
                 size="large"
