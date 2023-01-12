5c82ea0743b6539550ede19e586a35bba03f7fdb
Doncho Donev <donevdoncho92@gmail.com>
2023-01-12 08:33:53 +0200

    add new auth wrapper component to abstract login component

--- appointments/src/pages/Home.tsx
+++ appointments/src/pages/Home.tsx
@@ -1,19 +1,10 @@
-import { InteractionType } from "@azure/msal-browser";
-import { MsalAuthenticationTemplate } from "@azure/msal-react";
-import { loginRequest } from "../authConfig";
-import Error from "../components/Auth/Error";
-import Loading from "../components/Auth/Loading";
+import AuthWrapper from "../components/Auth/AuthWrapper";
 
 const Home: React.FC = () => {
   return (
-    <MsalAuthenticationTemplate
-      interactionType={InteractionType.Popup}
-      authenticationRequest={loginRequest}
-      errorComponent={Error}
-      loadingComponent={Loading}
-    >
-      <p>HOME</p>
-    </MsalAuthenticationTemplate>
+    <AuthWrapper>
+      <p>Home</p>
+    </AuthWrapper>
   );
 };
 
